61d4925cf3b314d935a724c381e2e42de4565b28
Jeong-Je <jeongjeson656@gmail.com>
2025-12-17 18:53:06 +0900

    sitemap 제대로 안 나오는 부분 수정

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,17 +1,27 @@
 import { MetadataRoute } from "next";
-import { allPages } from "contentlayer/generated";
+import { allPages, allBlogs } from "contentlayer/generated";
 
-const BASE_URL = "https://jeongje.vercel.app/";
+const BASE_URL = "https://jeongje.vercel.app"; // 나중에 커스텀 도메인 생기면 변경
 
 export default function sitemap(): MetadataRoute.Sitemap {
+  const pages = allPages.map((page) => ({
+    url: `${BASE_URL}/${page.slugAsParams}`,
+    lastModified: new Date(),
+  }));
+
+  const posts = allBlogs.map((post) => ({
+    url: `${BASE_URL}${post.slug}`,
+    lastModified: post.date
+      ? new Date(post.date)
+      : new Date(),
+  }));
+
   return [
     {
       url: BASE_URL,
       lastModified: new Date(),
     },
-    ...allPages.map((page) => ({
-      url: `${BASE_URL}/${page.slugAsParams}`,
-      lastModified: new Date()
-    })),
+    ...pages,
+    ...posts,
   ];
 }
